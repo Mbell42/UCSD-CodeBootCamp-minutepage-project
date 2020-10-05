e90effe9397740100501a97e08232052573817d9
Matthew Bell <matthewbell1030@gmail.com>
2020-10-04 21:51:47 -0700

    disabled sequelize content and functionality, as the owner closed their account. updated format of server.js.

--- server.js
+++ server.js
@@ -4,32 +4,53 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require('express');
+const express = require('express');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Requiring our models for syncing
-var db = require('./models');
+// MIDDLEWARE
+const app = express();
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Routes
-// =============================================================
-require('./routes/html-routes.js')(app);
-require('./routes/user-api-routes.js')(app);
-require('./routes/page-api-routes.js')(app);
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function () {
-  app.listen(PORT, function () {
-    console.log('App listening on PORT ' + PORT);
-  });
+
+// ROUTES
+app.use(require("./routes/html-routes.js"));
+
+
+// START SERVER
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}!`);
 });
+
+
+// // Requiring our models for syncing
+// // var db = require('./models');
+
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static directory
+// app.use(express.static('public'));
+
+// // Routes
+// // =============================================================
+// require('./routes/html-routes.js')(app);
+// // require('./routes/user-api-routes.js')(app);
+// // require('./routes/page-api-routes.js')(app);
+// // Syncing our sequelize models and then starting our Express app
+// // =============================================================
+// // db.sequelize.sync({ force: true })
+// // .then(
+//   const listener = () => {
+//   app.listen(PORT, function () {
+//     console.log('App listening on PORT ' + PORT);
+//   });
+// listener();
+// };
